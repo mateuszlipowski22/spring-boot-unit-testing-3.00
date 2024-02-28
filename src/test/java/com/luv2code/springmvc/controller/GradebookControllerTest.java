0379e6dea0ce486c432e042d61dda0683d53bae6
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2024-02-28 22:40:35 +0100

    adding test createANonValidGradeHttpRequestGradeTypeDoesNotExistEmptyResponse

--- src/test/java/com/luv2code/springmvc/controller/GradebookControllerTest.java
+++ src/test/java/com/luv2code/springmvc/controller/GradebookControllerTest.java
@@ -226,6 +226,20 @@ class GradebookControllerTest {
         ModelAndViewAssert.assertViewName(mav,"error");
     }
 
+    @Test
+    public void createANonValidGradeHttpRequestGradeTypeDoesNotExistEmptyResponse() throws Exception{
+        MvcResult mvcResult = this.mockMvc.perform(post("/grades")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .param("grade", "85.00")
+                        .param("gradeType", "literature")
+                        .param("studentId", "1"))
+                .andExpect(status().isOk()).andReturn();
+
+        ModelAndView mav = mvcResult.getModelAndView();
+
+        ModelAndViewAssert.assertViewName(mav,"error");
+    }
+
     @AfterEach
     public void setUpAfterTransactions(){
         jdbcTemplate.execute(sqlDeleteStudent);
